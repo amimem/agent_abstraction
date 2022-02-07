af8f4a631b13c831f4fd6ae3ffb0c5c67622aadf
Amin M <nil.objc@gmail.com>
2022-02-06 22:08:14 -0500

    add noisy net and n_step

---
+++ ray/rainbow_tune.py
@@ -0,0 +1,84 @@
+from ray import tune
+from ray.tune.registry import register_env
+from ray.rllib.env.wrappers.pettingzoo_env import PettingZooEnv
+from ray.rllib.env.wrappers.pettingzoo_env import ParallelPettingZooEnv
+from pettingzoo.magent import battle_v3
+from pettingzoo.utils import to_parallel
+from magent_wrappers import MAgengtPettingZooEnv, MAgentParallelPettingZooEnv
+import os
+import ray
+
+os.environ["TUNE_MAX_PENDING_TRIALS_PG"] = "1"
+
+# Based on code from github.com/parametersharingmadrl/parametersharingmadrl
+if __name__ == "__main__":
+    # RDQN - Rainbow DQN
+    # ADQN - Apex DQN
+
+    ray.init(include_dashboard=False, num_cpus=10, num_gpus=1, object_store_memory=60*10**9 ,_memory=90*10**9)
+    assert ray.is_initialized() == True
+
+    def env_creator(args):
+        env = battle_v3.env(map_size=15)
+        # env = to_parallel(env)
+        return MAgengtPettingZooEnv(env, flatten_dict_observations=False)
+
+    env = env_creator({})
+    register_env("battle_v3", env_creator)
+    print(set(env.agents), len(env.agents))
+
+    save_dir = os.getenv('SLURM_TMPDIR')
+
+    tune.run(
+        "DQN",
+        stop={"episodes_total": 5000},
+        local_dir=save_dir,
+        checkpoint_freq=500,
+        checkpoint_at_end=True,
+        config={
+            # Enviroment specific
+            "env": "battle_v3",
+            # "simple_optimizer":True,
+            # General
+            "framework": "torch",
+            "num_gpus": 1,
+            "num_workers": 9,
+            # "num_envs_per_worker": 0.25,
+            # "model": {"dim": 42, "conv_filters": [[16, [4, 4], 2], [32, [4, 4], 2], [512, [11, 11], 1]]},
+            # "num_atoms": 51
+            "noisy": True,
+            "n_step": 3,
+            # "v_min": -10,
+            # "v_max": 10,
+            "model": {
+                # "fcnet_hiddens": [64],
+                "dim": 15,
+                "conv_filters": [
+                    [32, [15, 15], 1],
+                    ],
+            },
+            # Method specific
+             "multiagent": {
+                "policies": set(env.agents),
+                "policy_mapping_fn": (
+                    lambda agent_id, episode, **kwargs: agent_id),
+                # Keep this many policies in the "policy_map" (before writing
+                # least-recently used ones to disk/S3).
+                # "policy_map_capacity": len(env.agents),
+                # Where to store overflowing (least-recently used) policies?
+                # Could be a directory (str) or an S3 location. None for using
+                # the default output dir.
+                # "policy_map_cache": save_dir,
+                # Function mapping agent ids to policy ids.
+            },
+            # "record_env": True,
+            # "train_batch_size": 600,
+            # "log_level": "DEBUG",
+            # "wandb": {
+            #     "project": "marl",
+            #     "api_key_file": "wandb_api_key.txt",
+            #     "log_config": True
+            # }
+        },
+        # callbacks=[WandbLoggerCallback(project="marl", api_key_file="wandb_api_key.txt",log_config=True)],
+        )
