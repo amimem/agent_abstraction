af01eaf58c97d3a5f95d92d198be77ca2bbfbb8b
Amin M <nil.objc@gmail.com>
2022-02-28 19:49:45 -0500

    add column selection

--- get_hdf5.py
+++ get_hdf5.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     parser.add_argument('--path', metavar='P', type=str, help='path')
     args = parser.parse_args()
 
-    jobid = int(os.getenv('SLURM_ARRAY_TASK_ID'))
+    jobid = 0
 
     j_path = f"{args.path}/jsons/game_tiple_presence_{jobid*100}_{(jobid+1)*100}_{jobid}.json"
     try:
@@ -30,6 +30,7 @@ if __name__ == "__main__":
         df = pd.DataFrame.from_records(all_records)
         df = df.dropna()
         df = df.reset_index(drop=True)
+        df = df[["game_id", "max_min_diff", "factor_same", "factor_diff"]]
         print("appended", j_path)
 
         try:
