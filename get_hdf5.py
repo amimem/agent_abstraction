f1f8b51920caf19c9d4e755e4d415a480670caa3
Amin M <nil.objc@gmail.com>
2022-03-02 19:19:38 -0500

    replace with new generator

--- get_hdf5.py
+++ get_hdf5.py
@@ -24,17 +24,17 @@ if __name__ == "__main__":
             game_triple_presence = json.load(file)
 
         all_records = []
-        for row in utils.gen_triple_rows(game_triple_presence):
+        for row in utils.gen_triple_window_rows(game_triple_presence):
             all_records.append(row.copy())
 
         df = pd.DataFrame.from_records(all_records)
-        df = df.dropna()
-        df = df.reset_index(drop=True)
-        df = df[["game_id", "max_min_diff", "factor_same", "factor_diff"]]
+        # df = df.dropna()
+        # df = df.reset_index(drop=True)
+        # df = df[["game_id", "max_min_diff", "factor_same", "factor_diff"]]
         print("appended", j_path)
 
         try:
-            h_path = f"{args.path}/hdf5/game_tiple_presence_{jobid*100}_{(jobid+1)*100}_{jobid}.h5"
+            h_path = f"{args.path}/hdf5_3/game_tiple_presence_{jobid*100}_{(jobid+1)*100}_{jobid}.h5"
             df.to_hdf(h_path, key='df', mode='w')
             print("saved", h_path)
         except Exception as e:
