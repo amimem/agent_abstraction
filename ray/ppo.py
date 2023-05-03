38b726299a798ea48e5b6334c94e19b6870930bb
amimem <nil.objc@gmail.com>
2023-05-03 15:22:37 -0400

    change to tag

--- ray/ppo.py
+++ ray/ppo.py
@@ -3,6 +3,7 @@ from ray.rllib.env.wrappers.pettingzoo_env import PettingZooEnv
 from ray.rllib.env.wrappers.pettingzoo_env import ParallelPettingZooEnv
 from pettingzoo.utils import to_parallel
 from pettingzoo.magent import battle_v3
+from pettingzoo.mpe import simple_tag_v2
 import ray.rllib.contrib.maddpg as maddpg
 from ray.rllib.agents.dqn import DQNTrainer, DQNTFPolicy, DQNTorchPolicy
 from ray.rllib.agents.ppo import PPOTrainer, PPOTFPolicy, PPOTorchPolicy
@@ -11,17 +12,19 @@ from ray.tune.registry import register_env
 import os
 from supersuit import flatten_v0
 
+os.environ["TUNE_MAX_PENDING_TRIALS_PG"] = "1"
+num_cpus = int(os.environ.get('SLURM_CPUS_PER_TASK'))
+
 if __name__ == "__main__":
 
-    ray.init(include_dashboard=False)
+    ray.init(include_dashboard=False, num_cpus=num_cpus)
 
     def env_creator(args):
-        env = battle_v3.env(map_size=15, minimap_mode=True)
-        env = flatten_v0(env)
+        env = simple_tag_v2.env(num_good=1, num_adversaries=3, num_obstacles=2, max_cycles=100, continuous_actions=False)
         return PettingZooEnv(env)
 
     env = env_creator({})
-    register_env("battle_v3", env_creator)
+    register_env("simple_tag", env_creator)
     print(set(env.agents))
 
     obs_space = env.observation_space
@@ -30,19 +33,19 @@ if __name__ == "__main__":
     # print(obs_space)
     # print(act_space)
 
-    policies = {
-        "ppo_policy": (PPOTorchPolicy, obs_space, act_space, {}),
-    }
+    # policies = {
+    #     "ppo_policy": (PPOTorchPolicy, obs_space, act_space, {}),
+    # }
 
-    print(policies)
+    # print(policies)
 
     ppo_trainer = PPOTrainer(
-        env="battle_v3",
+        env="simple_tag",
         config={
             "multiagent": {
-                "policies": policies,
-                "policy_mapping_fn":(
-                    lambda agent_id, episode, **kwargs: "ppo_policy"),
+                "policies": set(env.agents),
+                "policy_mapping_fn": (
+                    lambda agent_id, episode, **kwargs: agent_id),
             },
             # "model": {
                 # "fcnet_hiddens": [64]
@@ -58,7 +61,7 @@ if __name__ == "__main__":
             # minibatch size within each epoch.
             # "sgd_minibatch_size": 20,
             # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
-            "num_gpus": 2,
+            "num_gpus": 1,
             "num_workers": 10,
             "framework": "torch",
             # "log_level": "DEBUG",
@@ -66,10 +69,14 @@ if __name__ == "__main__":
 
     print(ppo_trainer)
 
-    for i in range(1000):
+    for i in range(5001):
         print("== Iteration", i, "==")
 
         # improve the PPO policy
         print("-- PPO --")
         result_ppo = ppo_trainer.train()
         print(pretty_print(result_ppo))
+
+        if i % 100 == 0:
+            checkpoint = ppo_trainer.save()
+            print("checkpoint saved at", checkpoint)
