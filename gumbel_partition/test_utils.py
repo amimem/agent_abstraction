e902d4007530152286016a1138176943cdf8d119
Amin M <nil.objc@gmail.com>
2024-02-21 19:59:00 -0500

    edit tests

--- gumbel_partition/test_utils.py
+++ gumbel_partition/test_utils.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from utils import MultiChannelNet
+from utils import MultiChannelNet, get_gumbel_softmax_sample
 
 # Create an instance of MultiChannelNet
 net = MultiChannelNet(n_channels=2, input_size=5, hidden_layer_width=10, n_hidden_layers=3, output_size=3)
@@ -21,3 +21,18 @@ print(output_tensor_with_output_dim.shape)  # Expected output: torch.Size([2, 2,
 net_without_output_dim = MultiChannelNet(n_channels=2, input_size=5, hidden_layer_width=10, n_hidden_layers=3, output_size=3)
 output_tensor_without_output_dim = net_without_output_dim.forward(input_tensor)
 print(output_tensor_without_output_dim.shape)  # Expected output: torch.Size([2, 2, 3])
+
+# Test the get_gumbel_softmax_sample function
+logit_vector = torch.randn(2, 3)
+tau = 0.5
+gumbel_softmax_sample = get_gumbel_softmax_sample(logit_vector, tau=tau)
+print(gumbel_softmax_sample.shape)  # Expected output: torch.Size([2, 3])
+
+# Test the get_gumbel_softmax_sample function for output differentiablity
+logit_vector = torch.randn(2, 3)
+tau = 0.5
+gumbel_softmax_sample = get_gumbel_softmax_sample(logit_vector, tau=tau, hard=True)
+print(gumbel_softmax_sample.shape)  # Expected output: torch.Size([2, 3])
+# The output tensor is differentiable
+gumbel_softmax_sample.sum().backward()
+print(logit_vector.grad)
